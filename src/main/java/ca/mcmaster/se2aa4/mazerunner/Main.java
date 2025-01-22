d871ab7e196d69195d4374a8efe49bbb508663c1
gbinijo <matthewgbinije@gmail.com>
2025-01-21 20:40:04 -0500

    Step 1

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -11,7 +11,18 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
+        
+        Options options = new Options();
+        options.addOptions("t", true, "Proper Input Required");
+
+        CommandLineParser parser = newDefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        if(cmd.hasOption("i")){
+            String inputFile = cmd.getOptionValue("i");
+        }
+
         try {
             System.out.println("**** Reading the maze from file " + args[0]);
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
@@ -19,18 +30,18 @@ public class Main {
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
                     if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
+                        logger.trace("WALL ");
                     } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
+                        logger.trace("PASS ");
                     }
                 }
-                System.out.print(System.lineSeparator());
+                logger.trace(System.lineSeparator());
             }
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.error("/!\\ An error has occured /!\\");
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+        logger.info("**** Computing path");
+        logger.warn("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
     }
 }
